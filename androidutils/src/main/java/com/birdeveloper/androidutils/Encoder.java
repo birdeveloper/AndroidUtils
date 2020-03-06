4fa2ae6fa1356970f99e86878b56b16a1a1bdaa3
gorkemkara <gorkem@stablemobile.com>
2020-03-06 16:48:01 +0300

    Add string encoder and decoder crypt

---
+++ androidutils/src/main/java/com/birdeveloper/androidutils/Encoder.java
@@ -0,0 +1,32 @@
+package com.wstat.util;
+
+
+
+public class Encoder {
+
+    private static final String key = "Zx" + Math.log(2) / 3;
+
+    public static String obfuscate(String s) {
+        char[] result = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            result[i] = (char) (s.charAt(i) + key.charAt(i % key.length()));
+        }
+
+        return new String(result);
+    }
+
+    public static String unObfuscate(String s) {
+        char[] result = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            result[i] = (char) (s.charAt(i) - key.charAt(i % key.length()));
+        }
+        String resultStr = new String(result);
+        try {
+            resultStr = new String(Base64.decode(resultStr), "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return resultStr;
+    }
+}
