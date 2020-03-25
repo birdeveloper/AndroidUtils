b81597668df1c637308a2d0b94e581d0e057e257
gorkemkara <info@gorkemkara.com.tr>
2020-03-25 15:28:31 +0300

    refresh library

--- androidutils/src/main/java/com/birdeveloper/androidutils/FileUtils.kt
+++
@@ -1,6 +0,0 @@
-package com.birdeveloper.androidutils
-
-import android.net.Uri
-import java.io.File
-
-fun File.toUri(): Uri = Uri.fromFile(this)
--- androidutils/src/main/java/com/birdeveloper/androidutils/ImageViewUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.birdeveloper.androidutils
-
-import android.widget.ImageView
-import java.io.File
-
-fun ImageView.setImageFile(imageFile: File) {
-    setImageURI(imageFile.toUri())
-}
